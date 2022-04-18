53ac9c26b9b9b3d13058951efd45ce0e1368fb65
Atticus-Robinson <acerobinson2@gmail.com>
2022-04-18 09:32:46 -0500

    starting on gets

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const noteRoutes = require("./routes/noteRoutes");
+const apiRoutes = require("./routes/apiRoutes/noteRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 
 const fs = require("fs");
 const express = require("express");
@@ -11,9 +13,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API Server is now on port ${PORT}~`);
-})
+  console.log(`API Server is now on port ${PORT}~`);
+});
